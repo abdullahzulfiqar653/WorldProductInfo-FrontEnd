b3ce1267c59d17175970a21674deb8d76e478b1b
abdullah <git@abdullahzulfiqar.me>
2022-08-04 15:06:53 +0500

    Initilizing the React App.

---
+++ app/src/App.js
@@ -0,0 +1,32 @@
+import "./App.css";
+import "./assets/css/darkdemo.min.css";
+import "./assets/css/demo-rtl.min.css";
+import "./assets/css/demo1.min.css";
+import "./assets/css/demo2.min.css";
+import "./assets/css/demo3.min.css";
+import "./assets/css/demo4.min.css";
+import "./assets/css/demo5.min.css";
+import "./assets/css/demo6.min.css";
+import "./assets/css/demo7.min.css";
+import "./assets/css/demo8.min.css";
+import "./assets/css/demo9.min.css";
+import "./assets/css/demo10.min.css";
+import "./assets/css/demo11.min.css";
+import "./assets/css/demo12.min.css";
+import "./assets/css/demo13.min.css";
+import "./assets/css/demo14.min.css";
+import "./assets/css/demo15.min.css";
+import "./assets/css/doc.min.css";
+import "./assets/css/style-rtl.min.css";
+import "./assets/css/style.min.css";
+import Home from "./components/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ app/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Navbar from "./Navbar";
+class Home extends Component {
+  render() {
+    return (
+      <div className="home">
+        <Navbar />
+      </div>
+    );
+  }
+}
+
+export default Home;
