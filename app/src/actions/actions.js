d66e696b24d1021e3d5d4aea42224e98297bdece
abdullah <git@abdullahzulfiqar.me>
2022-08-20 17:03:30 +0500

    added Pagination

--- app/src/actions/actions.js
+++ app/src/actions/actions.js
@@ -27,35 +27,36 @@ export const loadCategory = () => async (dispatch) => {
     });
 };
 
-export const productListLoaded = (categoryid, limit) => async (dispatch) => {
-  dispatch({
-    type: actions.REQUEST_START,
-  });
-  await axios
-    .get(
-      REQUEST_URL +
-        `/products/?categoryid=${categoryid}&flag=category&limit=${limit}`
-    )
-    .then((res) => {
-      // console.log(res.data);
-      if (res.data.count > 0) {
-        dispatch({
-          type: actions.PRODUCTS_LIST_LOADED,
-          payload: res.data,
-        });
-      } else {
+export const loadProductList =
+  (categoryid, limit, offset) => async (dispatch) => {
+    dispatch({
+      type: actions.REQUEST_START,
+    });
+    await axios
+      .get(
+        REQUEST_URL +
+          `/products/?categoryid=${categoryid}&flag=category&limit=${limit}&offset=${offset}`
+      )
+      .then((res) => {
+        // console.log(res.data);
+        if (res.data.count > 0) {
+          dispatch({
+            type: actions.PRODUCTS_LIST_LOADED,
+            payload: res.data,
+          });
+        } else {
+          dispatch({
+            type: actions.PRODUCTS_LIST_LOAD_FAIL,
+          });
+        }
+      })
+      .catch(function (error) {
+        // console.log(error);
         dispatch({
           type: actions.PRODUCTS_LIST_LOAD_FAIL,
         });
-      }
-    })
-    .catch(function (error) {
-      // console.log(error);
-      dispatch({
-        type: actions.PRODUCTS_LIST_LOAD_FAIL,
       });
-    });
-};
+  };
 
 export const productLoaded = (productid) => async (dispatch) => {
   dispatch({
---
+++ app/src/components/common/LargeLoader.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Oval } from "react-loader-spinner";
+
+function LargeLoader(props) {
+  return <Oval height="50" width="50" color="#336699" />;
+}
+
+export default LargeLoader;
